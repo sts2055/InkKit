3379eeb824d2967f5708d14b52f11c0f44169d41
Stefan Schmitt <sts2055@gmail.com>
2021-01-20 23:42:31 +0100

    fix target in package

--- Package.swift
+++ Package.swift
@@ -24,7 +24,7 @@ let package = Package(
         .target(
             name: "InkKit",
             dependencies: [],
-            path: "./GraphicsRenderer/Classes/"),
+            path: "./InkKit/Classes/"),
     ]
 )
 
